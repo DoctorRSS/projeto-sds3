6c7d12813c2b6612b5c2f780d71ca0033e83ba4f
DoctorRSS <silver.rob16@yahoo.com.br>
2021-05-04 23:22:56 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/dataTable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá Mundo!</h1>
+      <DataTable />
     </div>
+    <Footer/>
+    </>
   );
 }
 
